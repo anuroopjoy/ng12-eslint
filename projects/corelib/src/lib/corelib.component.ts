e76a319b87e5b8ad57c4ceabfb987d23df45da89
Anuroop Joy <anuroop.joy@hrblock.com>
2021-05-19 00:22:15 +0530

    init

---
+++ projects/corelib/src/lib/corelib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-corelib',
+  template: `
+    <p>
+      corelib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CorelibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
